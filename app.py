3753e516270efa2a9195c7f0dcb25f43e498e324
aepuertol <andrespuertolara@gmail.com>
2023-12-21 12:39:19 -0500

    new feats

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from collections import deque
 from dash_extensions.enrich import Trigger, FileSystemCache
 from tqdm import tqdm
 import time
-
+import pandas as pd
 
 def moving_average(x, span=100):
     return pd.DataFrame({'x': np.asarray(x)}).x.ewm(span=span).mean().values
@@ -19,13 +19,17 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
-fsc = FileSystemCache("cache_dir")
-fsc1 = FileSystemCache("cache_dir")
-fsc2 = FileSystemCache("cache_dir")
-fsc3 = FileSystemCache("cache_dir")
-fsc4 = FileSystemCache("cache_dir")
-fsc5 = FileSystemCache("cache_dir")
+fsc = FileSystemCache("cache_dir")# show frames
+fsc1 = FileSystemCache("cache_dir")# progressbar
+fsc2 = FileSystemCache("cache_dir")# stop flag
+fsc3 = FileSystemCache("cache_dir")# status warning
+fsc4 = FileSystemCache("cache_dir")# plot reward
+fsc5 = FileSystemCache("cache_dir") # text training progress
 
+suggested_params={
+    'CartPole':{'episodes':200,'batch_size':32,'memory_size':500,'gamma':0.95,'epsilon':1,'decrement':0.995,'lr':0.001},
+    'LunarLander':{'episodes':2000,'batch_size':64,'memory_size':10000,'gamma':0.995,'epsilon':1,'decrement':0.0005,'lr':0.001}
+}
 
 fsc1.set('progressbar','0')
 zeros_img=np.full((400,600,3),255,dtype=np.uint8)
@@ -33,19 +37,48 @@ fsc.set('images',zeros_img)
 fsc2.set('stop','0')
 fsc3.set('status','idle')
 fsc4.set('plotreward',[])
+fsc5.set('textparams',{'episode':0,'reward':0,'epsilon':0,'steps':0})
 app.layout = html.Div([
+    html.H4('Deep Reinforcement learning simulator'),
     html.Div(dcc.Dropdown(id='models-option',
         options=[
             {'label':'Lunar Lander','value':'LunarLander'},
             {'label':'Cart Pole','value':'CartPole'}],
-            value='LunarLander'
+            value='CartPole'
     )),
-    html.Div(id='env-div',children=[dcc.Graph(id='env-plot')]),
-    html.Div(id='reward-div',children=[dcc.Graph(id='reward-plot')]),
+    html.Div([
+            html.Div(id='discount-factor-div',children=[html.P('Gamma'),dcc.Input(id='discount_factor_input',type='text')]),
+            html.Div(id='epsilon-factor-div',children=[html.P('Epsilon'),dcc.Input(id='epsilon_input',type='text')]),
+            html.Div(id='epsilon-discount-div',children=[html.P('Decrement'),dcc.Input(id='epsilon_decrement_factor',type='text')]),
+            html.Div(id='learning-rate-div',children=[html.P('Learning rate'),dcc.Input(id='learning_rate',type='text')]),
+            html.Div(id='memory-size-div',children=[html.P('Replay memory size'),dcc.Input(id='memory_size',type='text')]),
+            html.Div(id='episodes-size-div',children=[html.P('Episodes'),dcc.Input(id='episode_size',type='text')]),
+            html.Div(id='batch-size-div',children=[html.P('Batch size'),dcc.Input(id='batch_size',type='text')])
+
+
+
+        ]),
+    html.Div([
+                
+        html.Div(id='env-div',children=[dcc.Graph(id='env-plot')],style={'display':'inline-block','width':'40%'}),
+        
+        html.Div(id='reward-div-plot',children=[dcc.Graph(id='reward-plot')],style={'display':'inline-block','width':'40%'}),
+        html.Div(id='text-div',
+            children=[
+                html.Div(id='episode-div'),
+                html.Div(id='reward-div'),
+                html.Div(id='epsilon-div'),
+                html.Div(id='steps-div')
+
+
+            ],style={'display':'inline-block','width':'20%'}
+        
+        )
+    ]),
     html.Div(html.Progress(id='progressbar',value='0',max='100')),
     html.Div(id='progressbar-msgs'),
     html.Div(html.Button('Train',id='train-btn',n_clicks=0)),
-    html.Div(html.Button('Reset',id='reset-btn',n_clicks=0)),
+    html.Div(html.Button('Stop',id='reset-btn',n_clicks=0)),
     html.Div(id='status-msg'),
     html.Div(id='msgs'),
     html.Div(id='stop-msg'),
@@ -66,7 +99,7 @@ def display_value(train_clicks,reset_clicks,model_option):
         if model_option=='LunarLander':
             episodes=2000
             batch_size=64
-            memory_size=100
+            memory_size=10000
             env=gym.make('LunarLander-v2',render_mode='rgb_array')
             agent=DeepQAgent3(env,max_memory_size=memory_size,learning_rate=1e-3,discount_factor=0.999,epsilon_greedy=1)
             state=env.reset()[0]
@@ -124,6 +157,8 @@ def display_value(train_clicks,reset_clicks,model_option):
                 steps+=1
                 if done:
                     total_rewards.append(rewards)
+                    fsc4.set('plotreward',total_rewards)
+                    fsc5.set('textparams',{'episode':e,'reward':rewards,'epsilon':agent.epsilon,'steps':steps})
                     print(f'Episode: {e} Total reward:{rewards} Epsilon:{agent.epsilon} steps: {steps}')
                     break
             fsc1.set('progressbar',str(100*(e+1)/episodes)) 
@@ -177,10 +212,34 @@ def stop_job(nclicksreset,nclickscontinue):
 #calback to status monitoring
 @callback(Output('status-msg','children'),
          [Input('statusloader','n_intervals')])
-def show_status(status):
+def show_status(statusintervals):
     value=fsc3.get('status')
     return value
 #callback to plot reward
+@callback(Output('reward-plot','figure'),
+          [Input('statusloader','n_intervals')])
+def show_reward(plotintervals):
+    total_rewards=fsc4.get('plotreward')
+    fig=go.Figure()
+    fig.add_trace(go.Scatter(y=total_rewards,mode='lines+markers',name='Total reward'))
+    fig.add_trace(go.Scatter(y=moving_average(total_rewards),name='Mean reward'))
+    fig.update_xaxes(title='Episodes')
+    fig.update_yaxes(title='Reward')
+    return fig
+#callback to monitoring parameters
+@callback(Output('episode-div','children'),
+          Output('reward-div','children'),
+          Output('epsilon-div','children'),
+          Output('steps-div','children'),
+         [Input('statusloader','n_intervals')])
+def show_parameters(parameters_interval):
+    vals=fsc5.get('textparams')
+    episode=vals['episode']
+    rewards=vals['reward']
+    epsilon=vals['epsilon']
+    steps=vals['steps']
+    return f'Episode : {episode}',f'Reward: {rewards}',f'Epsilon: {epsilon}',f'Steps: {steps}'
+
 
 
 
